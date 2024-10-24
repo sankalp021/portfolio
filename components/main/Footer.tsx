a0857fff91489040e9ce4e7e3c0b456acb9364d6
sankalp021 <105117044+sankalp021@users.noreply.github.com>
2024-10-25 01:34:46 +0530

    error fixed

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -6,44 +6,91 @@ import {
   RxTwitterLogo,
   RxLinkedinLogo,
 } from "react-icons/rx";
-
 import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+    <div className="w-full h-auto bg-transparent text-gray-200 shadow-lg p-[15px] relative z-[20]">
       <div className="w-full flex flex-col items-center justify-center m-auto">
         <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+          
+          {/* Community Section */}
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start relative z-[30]">
             <div className="font-bold text-[16px]">Community</div>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <RxGithubLogo />
-              <span className="text-[15px] ml-[6px]">sankalp021</span>
-            </p>
-          </div>
-
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">Social Media</div>
             <a
-              href="https://www.linkedin.com"
+              href="https://github.com/sankalp021"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-row items-center my-[15px] cursor-pointer "
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxGithubLogo />
+              <span className="text-[15px] ml-[6px]">sankalp021</span>
+            </a>
+            {/* <a
+              href="https://discord.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxDiscordLogo />
+              <span className="text-[15px] ml-[6px]">Discord</span>
+            </a> */}
+          </div>
+
+          {/* Social Media Section */}
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start relative z-[30]">
+            <div className="font-bold text-[16px]">Social Media</div>
+            <a
+              href="https://www.linkedin.com/in/sankalp-chaturvedi-567315233 "
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
             >
               <RxLinkedinLogo />
               <span className="text-[15px] ml-[6px]">Sankalp Chaturvedi</span>
             </a>
+            {/* <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxTwitterLogo />
+              <span className="text-[15px] ml-[6px]">Twitter</span>
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <RxInstagramLogo />
+              <span className="text-[15px] ml-[6px]">Instagram</span>
+            </a> */}
+            {/* <a
+              href="https://www.youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <FaYoutube />
+              <span className="text-[15px] ml-[6px]">YouTube</span>
+            </a> */}
           </div>
 
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+          {/* About Section */}
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start relative z-[30]">
             <div className="font-bold text-[16px]">About</div>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+            <a
+              href="mailto:snklp21@gmail.com"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
               <span className="text-[15px] ml-[6px]">snklp21@gmail.com</span>
-            </p>
+            </a>
           </div>
         </div>
 
-        <div className="mb-[20px] text-[15px] text-center">
+        <div className="mb-[20px] text-[15px] text-center relative z-[30]">
           &copy; Made by Sankalp021
         </div>
       </div>
--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -4,28 +4,51 @@ import ProjectCard from "../sub/ProjectCard";
 const Projects = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center py-20"
+      className="flex flex-col items-center z-20 justify-center py-20"
       id="projects"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/bkss.png"
-          title="BranchKarma Website"
-          description=" "
-        />
-        <ProjectCard
-          src="/jpss.png"
-          title="Job portal website"
-          description=" "
-        />
-        <ProjectCard
-          src="/msss.png"
-          title="Mystore website"
-          description=" "
-        />
+        <a
+          href="https://bk-sankalp.netlify.app/" // Replace with the actual URL of the project
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex-1"
+        >
+          <ProjectCard
+            src="/bkss.png"
+            title="BranchKarma Website"
+            description="A responsive website built for BranchKarma, focusing on user experience and seamless integration with GoHighLevel for enhanced CRM functionalities."
+          />
+        </a>
+        
+        <a
+          href="https://jobport-nine.vercel.app/" // Replace with the actual URL of the project
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex-1"
+        >
+          <ProjectCard
+            src="/jpss.png"
+            title="Job Portal Website"
+            description="A platform that connects job seekers and employers, featuring user registration, job postings, and a streamlined application process."
+          />
+        </a>
+        
+        <a
+          href="https://mystoresanklap.netlify.app/" // Replace with the actual URL of the project
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex-1"
+        >
+          <ProjectCard
+            src="/msss.png"
+            title="Mystore Website"
+            description="An e-commerce platform designed to provide a smooth shopping experience, complete with product listings, a shopping cart, and secure checkout."
+          />
+        </a>
       </div>
     </div>
   );
